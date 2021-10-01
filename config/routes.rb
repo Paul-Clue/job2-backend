da9076b6e85273dee5eb413df9390b6fe442144d
unknown <paulclue20@gmail.com>
2021-09-30 20:10:03 -0500

    Add root route for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :expressions
 
   get 'expression' => 'expressions#expression'
+  get '/' => 'expressions#expression'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
